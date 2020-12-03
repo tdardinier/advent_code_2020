920a1c739d46dad6a2b6e8b47b9b20ea9e918238
Thibault Dardinier <tdardini@student.ethz.ch>
2020-12-03 17:03:52 +0100

    One fewer line for 3

--- 3/main.py
+++ 3/main.py
@@ -1,12 +1,11 @@
 M = [[x == "#" for x in l[:-1]] for l in open("input.txt", "r").readlines()]
 
-(slopes, m) = ([(3, 1), (1, 1), (5, 1), (7, 1), (1, 2)], 1)
+(slopes, m) = ([(1, 1), (5, 1), (7, 1), (1, 2), (3, 1)], 1)
 
-for (i, (sx, sy)) in enumerate(slopes):
+for (sx, sy) in slopes:
     (x, y, c) = (0, 0, 0)
     while y < len(M):
         (c, x, y) = (c + M[y][x % len(M[0])], x + sx, y + sy)
-    if i == 0: print(c)
     m *= c
 
-print(m)
+print(c, m)
